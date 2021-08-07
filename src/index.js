e8347a523672ff8e180bd244a603070c6cd4e045
dulabdul <dul.abdoel21@gmail.com>
2021-08-08 05:02:56 +0700

    entry point

--- src/index.js
+++ src/index.js
@@ -3,3 +3,16 @@ import './styles/app.scss';
 import './styles/style.css';
 import 'bootstrap';
 import 'regenerator-runtime';
+import $ from 'jquery';
+import 'lazysizes';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+// import component
+import './scripts/component/navbar';
+import './scripts/component/konten-bar';
+import './scripts/component/footer-bar';
+
+$(document).ready(() => {
+  $('body').scrollspy({
+    target: '#nav-area',
+  });
+});
